4485c557f9a2e331ec0ce46a4071871f101a65e5
Dmitriy Borisov <borisovdv1994@ya.ru>
2022-05-05 23:14:51 +0400

    fix

--- src/components/UI/StyledNoteTextare.jsx
+++ src/components/UI/StyledNoteTextare.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 import { StyledTextarea } from './StyledTextarea'
 
-export const StyledNoteTextarea = styled(StyledTextarea)`
+export const StyledNoteTextarea = styled.textarea`
+resize:none;
+padding:.5rem;
+border-radius:0.5rem;
+width:100%;
 min-height:100px;
+border:none;
+outline:none;
 background:transparent;
 color:${props => props.currentTheme === 'light'
-      ? props.theme.colors.text.dark
-      : props.theme.colors.text.light};
+            ? props.theme.colors.text.dark
+            : props.theme.colors.text.light};
 `
